640c577b81f3385dfb1ae303e50c467028dfd0de
giovana.borges <gigi12345as17@gmail.com>
2023-09-05 16:56:46 -0300

    :feat create bin for restTemplate and controllerException to handle with exceptions

---
+++ challenge/src/main/java/com/backend/challenge/config/ControllerExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.backend.challenge.config;
+
+import com.backend.challenge.DTO.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDTO exceptiondDTO = new ExceptionDTO("Usuario já cadastrado","400");
+        return ResponseEntity.badRequest().body(exceptiondDTO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+}
