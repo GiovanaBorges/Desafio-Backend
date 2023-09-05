cfcfd702d20427d8c4f676b904bdf9fd2354573e
giovana.borges <gigi12345as17@gmail.com>
2023-09-05 16:53:23 -0300

    :feat created repository for user and transaction

---
+++ challenge/src/main/java/com/backend/challenge/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.backend.challenge.repository;
+
+import com.backend.challenge.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+}
